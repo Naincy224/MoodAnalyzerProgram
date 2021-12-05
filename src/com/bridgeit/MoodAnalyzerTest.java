3c50ccb559a229331f40b27a7013090bb69ad713
Naincy224 <pihusingh224@gmail.com>
2021-12-05 09:22:24 +0530

    Modified: Added Massege in constructou for sad and happy

--- src/com/bridgeit/MoodAnalyzerTest.java
+++ src/com/bridgeit/MoodAnalyzerTest.java
@@ -8,21 +8,23 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
 
 	public static MoodAnalyzer analyser;
-	public String message = "i am in sad Mood";
+	public String message1 = "i am in sad Mood";
+	public String message2 = "i am in happy Mood";
 	
 	@BeforeClass
 	public static void init() {
 		analyser = new MoodAnalyzer();
 	}
-	
+	//Test case refactor1.1
 	@Test
 	public void testMessageToReturnSad() {
-		String result = analyser.analyseMood(message);
+		String result = analyser.analyseMood(message1);
 		assertEquals(MoodAnalyzer.first_mood, result);
 	}
+	//Test case refactor1.2
 	@Test
 	public void testMessageToReturnHappy() {
-		String result = analyser.analyseMood(message);
+		String result = analyser.analyseMood(message2);
 		assertEquals(MoodAnalyzer.second_mood, result);
 	}
 	
